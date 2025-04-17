f79926c721f4f53980a23a0b0e512a2f8242fdf4
Yuya-KASHIYAMA <167611270+Yuya-KASHIYAMA@users.noreply.github.com>
2025-04-18 00:08:52 +0900

    Add files via upload

---
+++ trees.js
@@ -0,0 +1,98 @@
+const trees = [
+  {
+    "id": "tree001",
+    "species": "マツ",
+    "difficulty": 5,
+    "size": "大",
+    "x": 0.5428,
+    "y": 0.6951,
+    "photos": [],
+    "comment": "信じられない形してる"
+  },
+  {
+    "id": "tree002",
+    "species": "イチョウ",
+    "difficulty": 4,
+    "size": "中",
+    "x": 0.5286,
+    "y": 0.592,
+    "photos": [
+      "https://i.postimg.cc/pLZ6KkD5/1720494892598.jpg"
+    ],
+    "comment": ""
+  },
+  {
+    "id": "tree003",
+    "species": "？？？",
+    "difficulty": 2,
+    "size": "中",
+    "x": 0.1071,
+    "y": 0.3227,
+    "photos": [
+      "https://i.postimg.cc/13Gc5Yn0/image.png",
+      "https://i.postimg.cc/Ls0VrggG/image.png"
+    ],
+    "comment": "ブランコがある"
+  },
+  {
+    "id": "tree004",
+    "species": "？？？",
+    "difficulty": 3,
+    "size": "中",
+    "x": 0.4658,
+    "y": 0.7935,
+    "photos": [
+      "https://i.postimg.cc/wBg7bYJR/image.png",
+      "https://i.postimg.cc/nz5B73zL/image.png"
+    ],
+    "comment": ""
+  },
+  {
+    "id": "tree005",
+    "species": "モチノキ",
+    "difficulty": 2,
+    "size": "中",
+    "x": 0.4494,
+    "y": 0.7153,
+    "photos": [
+      "https://i.postimg.cc/hGDt1bW9/image.png"
+    ],
+    "comment": ""
+  },
+  {
+    "id": "tree006",
+    "species": "ボダイジュ",
+    "difficulty": 3,
+    "size": "中",
+    "x": 0.6751,
+    "y": 0.5463,
+    "photos": [
+      "https://i.postimg.cc/2518tjy6/image.png"
+    ],
+    "comment": ""
+  },
+  {
+    "id": "tree007",
+    "species": "サクラ",
+    "difficulty": 2,
+    "size": "小",
+    "x": 0.7628,
+    "y": 0.551,
+    "photos": [
+      "https://i.postimg.cc/CxXVzGMh/image.png"
+    ],
+    "comment": ""
+  },
+  {
+    "id": "tree008",
+    "species": "？？？",
+    "difficulty": 5,
+    "size": "大",
+    "x": 0.7716,
+    "y": 0.5776,
+    "photos": [
+      "https://i.postimg.cc/0jDMSxFt/image.png"
+    ],
+    "comment": ""
+  }
+];
