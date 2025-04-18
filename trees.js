f2f070dececb473d3b20ea200c385f4a7e4040a4
Yuya-KASHIYAMA <167611270+Yuya-KASHIYAMA@users.noreply.github.com>
2025-04-18 23:49:51 +0900

    Add files via upload

---
+++ trees.js
@@ -0,0 +1,150 @@
+const trees = [
+  {
+    "id": "tree001",
+    "species": "マツ",
+    "difficulty": 5,
+    "size": "大",
+    "x": 0.5428,
+    "y": 0.6951,
+    "photos": [
+      "https://i.postimg.cc/wxrpPvfP/image.png",
+      "https://i.postimg.cc/FKTqLWwq/image.png"
+    ],
+    "comment": "でかすぎ、形すごい"
+  },
+  {
+    "id": "tree002",
+    "species": "イチョウ",
+    "difficulty": 4,
+    "size": "中",
+    "x": 0.5286,
+    "y": 0.592,
+    "photos": [
+      "https://i.postimg.cc/pLZ6KkD5/1720494892598.jpg"
+    ],
+    "comment": ""
+  },
+  {
+    "id": "tree003",
+    "species": "？？？",
+    "difficulty": 2,
+    "size": "中",
+    "x": 0.1071,
+    "y": 0.3227,
+    "photos": [
+      "https://i.postimg.cc/13Gc5Yn0/image.png",
+      "https://i.postimg.cc/Ls0VrggG/image.png"
+    ],
+    "comment": "ブランコがある"
+  },
+  {
+    "id": "tree004",
+    "species": "？？？",
+    "difficulty": 3,
+    "size": "中",
+    "x": 0.4658,
+    "y": 0.7935,
+    "photos": [
+      "https://i.postimg.cc/wBg7bYJR/image.png",
+      "https://i.postimg.cc/pTqFx8hK/image.png"
+    ],
+    "comment": "正門を通る人が良く見える"
+  },
+  {
+    "id": "tree005",
+    "species": "モチノキ",
+    "difficulty": 2,
+    "size": "中",
+    "x": 0.4084,
+    "y": 0.7221,
+    "photos": [
+      "https://i.postimg.cc/hGDt1bW9/image.png"
+    ],
+    "comment": ""
+  },
+  {
+    "id": "tree006",
+    "species": "ボダイジュ",
+    "difficulty": 3,
+    "size": "中",
+    "x": 0.6751,
+    "y": 0.5463,
+    "photos": [
+      "https://i.postimg.cc/2518tjy6/image.png"
+    ],
+    "comment": ""
+  },
+  {
+    "id": "tree007",
+    "species": "サクラ",
+    "difficulty": 2,
+    "size": "小",
+    "x": 0.7542,
+    "y": 0.5541,
+    "photos": [
+      "https://i.postimg.cc/CxXVzGMh/image.png"
+    ],
+    "comment": ""
+  },
+  {
+    "id": "tree008",
+    "species": "？？？",
+    "difficulty": 5,
+    "size": "大",
+    "x": 0.7716,
+    "y": 0.5776,
+    "photos": [
+      "https://i.postimg.cc/0jDMSxFt/image.png"
+    ],
+    "comment": ""
+  },
+  {
+    "id": "tree009",
+    "species": "？？？",
+    "difficulty": 4,
+    "size": "中",
+    "x": 0.2014,
+    "y": 0.5542,
+    "photos": [
+      "https://i.postimg.cc/tJq8n04L/image.png"
+    ],
+    "comment": "テニスボールが挟まっていがちなエリア"
+  },
+  {
+    "id": "tree010",
+    "species": "？？？",
+    "difficulty": 3,
+    "size": "中",
+    "x": 0.5288,
+    "y": 0.3922,
+    "photos": [
+      "https://i.postimg.cc/yYzw2bCq/image.png",
+      "https://i.postimg.cc/fTP3TDHS/image.png"
+    ],
+    "comment": ""
+  },
+  {
+    "id": "tree011",
+    "species": "？？？",
+    "difficulty": 3,
+    "size": "中",
+    "x": 0.7564,
+    "y": 0.545,
+    "photos": [
+      "https://i.postimg.cc/76dHrMMC/image.png"
+    ],
+    "comment": ""
+  },
+  {
+    "id": "tree012",
+    "species": "？？？",
+    "difficulty": 2,
+    "size": "小",
+    "x": 0.7535,
+    "y": 0.3551,
+    "photos": [
+      "https://i.postimg.cc/wjmDC0XN/image.png"
+    ],
+    "comment": "実がなる"
+  }
+];
